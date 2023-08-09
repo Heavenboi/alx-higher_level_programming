322f9827c04d7eed9c825386b96c3ba659095842
Heavenboi <nigelzenda98@gmail.com>
2023-08-09 04:58:22 -0700

    fixed some errors

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-    print(chr(letter), end='')
+letters = ""
+for i in range(ord('a'), ord('z') + 1):
+    letters += chr(i)
+print('{}'.format(letters), end="")
