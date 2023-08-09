a0ef550f72cc893cd68596685dc946febf747964
Heavenboi <nigelzenda98@gmail.com>
2023-08-09 07:05:06 -0700

    removed the second print fuction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,4 @@ def uppercase(str):
     for char in str:
         if 'a' <= char <= 'z':
             char = chr(ord(char) - ord('a') + ord('A'))
-        print(char, end="")
-    print()
+        print(char, end="\n")
