67479ec3cf366ef169ad67efcb7f8f654b6e44db
Heavenboi <nigelzenda98@gmail.com>
2023-08-29 14:36:36 -0700

    removed som trailling white space

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-''' 
+'''
 This module defines an empty class Square that represents a square shape.
-
-The Square class does not have any attributes or methods defined. It serves as a blueprint
-for creating square objects with no specific functionality or data.
-
 '''
 
 
