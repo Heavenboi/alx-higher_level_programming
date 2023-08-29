e1a45f0afcb976906cd84f0a93f7533a0a1018b8
Heavenboi <nigelzenda98@gmail.com>
2023-08-29 14:23:51 -0700

    fixed docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-''' square class that does not return anything '''
+''' 
+This module defines an empty class Square that represents a square shape.
+
+The Square class does not have any attributes or methods defined. It serves as a blueprint
+for creating square objects with no specific functionality or data.
+
+'''
 
 
 class Square:
