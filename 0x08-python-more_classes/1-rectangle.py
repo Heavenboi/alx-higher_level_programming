d5ecdc46828af7163b11696804c1b2724c343149
Heavenboi <nigelzenda98@gmail.com>
2023-09-04 08:41:55 -0700

    a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """
-Defines a Rectangle class.
+Creates instances of the Rectangle class and prints their attribute dictionaries.
 """
-
 
 class Rectangle:
     """
@@ -20,58 +19,8 @@ class Rectangle:
         self.width = width
         self.height = height
 
-    @property
-    def width(self):
-        """
-        Retrieves the width of the rectangle.
 
-        Returns:
-            int: The width of the rectangle.
-        """
-        return self.__width
+my_rectangle.width = 10
+my_rectangle.height = 3
 
-    @width.setter
-    def width(self, value):
-        """
-        Sets the width of the rectangle.
-
-        Args:
-            value (int): The new width of the rectangle.
-
-        Raises:
-            TypeError: If width is not an integer.
-            ValueError: If width is less than 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """
-        Retrieves the height of the rectangle.
-
-        Returns:
-            int: The height of the rectangle.
-        """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """
-        Sets the height of the rectangle.
-
-        Args:
-            value (int): The new height of the rectangle.
-
-        Raises:
-            TypeError: If height is not an integer.
-            ValueError: If height is less than 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+my_rectangle = Rectangle(2, 4)
