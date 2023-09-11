109b37700bdd74177f472d082ddf56e1b254981a
Heavenboi <nigelzenda98@gmail.com>
2023-09-11 14:11:33 -0700

    corrected pycode on task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 """
-a fuction that reatunrs a list of available attributes of an object
+a fuction that returns a list of available attributes of an object
 """
 
+
 def lookup(obj):
+    """
+    the function that returns the attrubutes of the object
+    """
     return dir(obj)
