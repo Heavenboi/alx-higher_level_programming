fd9042f73995c8449b45bf00a3715302de14df78
Heavenboi <nigelzenda98@gmail.com>
2023-10-18 22:34:08 +0200

    fixed bugs

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
--- Create user and write SELECT privilege on database
+-- Create user and write SELECT privilege on database.
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
